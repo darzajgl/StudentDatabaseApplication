5951b9d34d484d30bc34f3ca069e32675337a909
Dariusz Zajglic <darzajgl@gmail.com>
2022-03-23 22:03:18 +0100

    Enroll in courses

---
+++ src/StudentDatabaseApplication.java
@@ -0,0 +1,12 @@
+public class StudentDatabaseApplication {
+
+    public static void main(String[] args) {
+        Student stud1 = new Student();
+        stud1.enroll();
+
+
+        //Ask how many new user we want to add
+
+        // Create n number of new students
+    }
+}
