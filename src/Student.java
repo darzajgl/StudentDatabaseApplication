c443e0f32253fadd10ad07ed85ef21341ff28141
Dariusz Zajglic <darzajgl@gmail.com>
2022-03-23 22:07:58 +0100

    Enroll in courses 2

--- src/Student.java
+++ src/Student.java
@@ -46,7 +46,7 @@ public class Student {
         System.out.println("Enter course to enroll (Q to quit): ");
         Scanner in = new Scanner (System.in);
         String course = in.nextLine();
-        if(course != "Q") {
+        if(!course.equals("Q")) {
             courses = courses +"\n" + course;
             tuitionBalance = tuitionBalance +  costOfCourse;
         }
